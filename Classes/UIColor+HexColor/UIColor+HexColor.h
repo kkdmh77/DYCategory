41e7fe1dad9e64d5cbbf2816315683f214ad3e83
shuleihen <shuleihen@126.com>
2017-02-10 09:45:04 +0800

    添加 json和hexcolor分类

---
+++ Classes/UIColor+HexColor/UIColor+HexColor.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+HexColor.h
+//  LLInstalmentSDK
+//
+//  Created by zdy on 2017/2/8.
+//  Copyright © 2017年 lianlian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HexColor)
++ (UIColor *)colorWithHexString:(NSString *)color;
+@end
